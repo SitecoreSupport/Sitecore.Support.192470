02255269cce70927818408b7e2e6416862aabfff
DK\IEKA <ieka@sitecore.net>
2017-11-15 14:02:39 +0200

    Remove unnecessary view engines

---
+++ src/Sitecore.Support.192470/Pipelines/HttpRequest/ViewEngineRemoving.cs
@@ -0,0 +1,31 @@
+﻿using RazorGenerator.Mvc;
+using Sitecore.Pipelines;
+using Sitecore.Pipelines.HttpRequest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sitecore.Support.Pipelines.HttpRequest
+{
+    public class ViewEngineRemoving: HttpRequestProcessor
+    {
+        private static bool flag = false;
+        public override void Process(HttpRequestArgs args)
+        {
+            if (!flag)
+            {
+                for (int i = 0; i < ViewEngines.Engines.Count; i++)
+                {
+                    var precompiledViewEngine = ViewEngines.Engines[0] as PrecompiledMvcEngine;
+                    if (precompiledViewEngine != null)
+                    {
+                        ViewEngines.Engines.Remove(precompiledViewEngine);
+                        flag = true;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.192470/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.192470")]
+[assembly: AssemblyProduct("Sitecore.Support.192470")]
+[assembly: ComVisible(false)]
